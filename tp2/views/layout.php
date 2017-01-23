fb7574a13957f464d16935acf70f3649758cc4a4
forget59 <fabrice.forget.59@gmail.com>
2017-01-23 14:53:40 +0100

    tp2

---
+++ tp2/views/layout.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <header>
+      <a href='/astonS4/aston4/tp2'>Home</a>
+    </header>
+    <?php require_once('routes.php'); ?>
+    <footer>
+      Copyright
+    </footer>
+  <body>
+<html>
